75113fbc9ca237aa1eff529f85cd4be6f75be30a
Oscar Ramirez <osramirezf@gmail.com>
2023-05-09 20:02:48 -0400

    feat: Agregamos funcion para login y registro de usuario

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,9 +1,50 @@
 
-import express from "express";
+import express from 'express';
+import { getUserByEmail, createUser } from '../db/users';
+import { authentication, random } from '../helper';
 
-import { createUser, getUsersEmail } from "db/users";
-import { authentication, random } from "helpers";
+//Para inicio de sesion
 
+export const login = async (req: express.Request, res: express.Response) => {
+    try {
+
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.sendStatus(400);
+        }
+
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+        if (!user) {
+            return res.sendStatus(400);
+        }
+
+        const expectedHash = authentication(user.authentication.salt, password);
+
+        if (user.authentication.password !== expectedHash) {
+            return res.sendStatus(403);
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+        await user.save();
+
+        res.cookie('REST-API-MONGO', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
+        
+        return res.status(200).json(user).end();
+
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+
+
+
+}
+
+
+//Para registro de usuario
 export const register = async (req: express.Request, res: express.Response) => {
     try {
         const { email, password, username } = req.body;
@@ -12,7 +53,7 @@ export const register = async (req: express.Request, res: express.Response) => {
             return res.sendStatus(400);
         }
 
-        const exitingUser = await getUsersEmail(email);
+        const exitingUser = await getUserByEmail(email);
 
         if (exitingUser) {
             return res.sendStatus(400);
@@ -28,7 +69,6 @@ export const register = async (req: express.Request, res: express.Response) => {
         });
 
         return res.status(200).json(user).end();
-
 
     } catch (error) {
         console.log(error);
