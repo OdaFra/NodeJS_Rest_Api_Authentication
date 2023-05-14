79f75f44b9b124f43c9209c5c723430a68061305
Oscar Ramirez <osramirezf@gmail.com>
2023-05-13 20:39:54 -0400

    route: Agregamos ruta para users.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import authentiction from './authentiction';
-
+import users from './users';
 
 const router = express.Router();
 
 export default (): express.Router=>{
     authentiction(router);
+    users(router);
     return router;
 }
