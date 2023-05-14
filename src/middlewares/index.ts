24f30b3c6a6ede5c6dd5a6b3302990b6a7d3190d
Oscar Ramirez <osramirezf@gmail.com>
2023-05-13 20:48:17 -0400

    feat: Agregamos funcion para retornar mensaje si un usuario no se encuentra en la base de datos.

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,6 +3,29 @@ import { get, merge } from "lodash";
 
 import { getUserBySessionToken } from "../db/users";
 
+export const isOwner = async(req:express.Request, res:express.Response, next:express.NextFunction)=>{
+    try {
+      const {id}=req.params;
+      const currentUserId = get(req, 'identity._id') as string;
+
+      if(!currentUserId){
+        return res.sendStatus(403).json({
+            message:'Usuario no identificado en la base de datos'
+        });
+      }
+      if(currentUserId.toString()!==id){
+        return res.sendStatus(403).json({
+            message:'Usuario no identificado en la base de datos'
+        });
+      }
+      return next();
+
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
         const sessionToken = req.cookies['REST-API-MONGO'];
